8a0203070c46f6422269df0cb72309fa25c72169
linai <918940331@qq.com>
2022-08-15 17:11:23 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,268 @@
+import tkinter as tk
+import tkinter.font as tkFont
+import tkinter.ttk
+import tkinter.messagebox
+import cv2
+from tkinter import filedialog
+
+
+# 打开系统文件路径方法
+
+
+# 主窗口
+class master:
+    def __init__(self, root):
+        self.root = root
+        self.root.geometry('%dx%d' % (1920, 1080))
+        self.root.title('视频标记工具--10')
+        frame1(self.root)
+
+
+# 第一个页面
+class frame1:
+    def __init__(self, root):
+        self.root = root
+        self.frame1 = tk.Frame(root)
+        self.frame1.pack()
+        self.username = tk.StringVar()
+
+        self.password = tk.StringVar()
+        # 控件
+
+        # 字体
+        fontStyle = tkFont.Font(family="Lucida Grande", size=20)
+
+        fontStyle2 = tkFont.Font(family="Lucida Grande", size=30)
+        # 标题
+        title = tk.Label(self.frame1, text='登 录Sky_AGI ', font=fontStyle2)
+        # 用户名输入
+        label1 = tk.Label(self.frame1, text='username: ', font=fontStyle)
+
+        entry1 = tk.Entry(self.frame1, textvariable=self.username, font=fontStyle)
+        # 密码输入
+        label2 = tk.Label(self.frame1, text='password: ', font=fontStyle)
+        entry2 = tk.Entry(self.frame1, textvariable=self.password, font=fontStyle)
+        # 功能按钮
+        button1 = tk.Button(self.frame1, text='确认', font=fontStyle, command=self.login)
+        button2 = tk.Button(self.frame1, text='退出', font=fontStyle, command=self.quit)
+
+        # 布局
+        title.grid(column=2, row=0)
+        label1.grid(column=1, row=1)
+        entry1.grid(column=2, row=1)
+
+        label2.grid(column=1, row=2)
+        entry2.grid(column=2, row=2)
+
+        button1.grid(column=1, row=3)
+        button2.grid(column=2, row=3)
+
+    def login(self):
+        if True:
+            self.frame1.destroy()
+            frame2(self.root)
+
+    def quit(self):
+        root.destroy()
+
+
+class frame2:
+
+    # 第二个页面2:
+    def __init__(self, root):
+        self.root = root
+        self.frame2 = tk.Frame(root)
+        self.frame2.pack()
+
+        fontStyle = tkFont.Font(family="Lucida Grande", size=20)
+
+        button1 = tk.Button(self.frame2, text='常规', font=fontStyle, command=self.login_frame3)
+        button2 = tk.Button(self.frame2, text='高密', font=fontStyle, command=self.login_frame4)
+        button3 = tk.Button(self.frame2, text='退出', font=fontStyle, command=self.login_frame1)
+
+        #
+        button1.grid(column=1, row=2)
+        button2.grid(column=1, row=3)
+        button3.grid(column=1, row=4)
+
+
+    def login_frame3(self):
+        self.frame2.destroy()
+        frame3(self.root)
+
+    def login_frame4(self):
+        self.frame2.destroy()
+        frame4(self.root)
+    def login_frame1(self):
+        self.frame2.destroy()
+        frame1(self.root)
+
+
+
+# 第三个页面 常规
+class frame3:
+    def __init__(self, root):
+        self.root = root
+        self.frame3 = tk.Frame(root)
+        self.frame3.pack()
+        # 所有控件
+        event_str = tk.StringVar()
+        fontStyle = tkFont.Font(family="Lucida Grande", size=20)
+
+        title = tk.Label(self.frame3, text='Ai视频分析系统', font=fontStyle)
+        # 画布
+        canvas1 = tk.Canvas(self.frame3, width=1728, height=972, bg='white')
+        # notebook1
+        frameOne = tkinter.Frame()
+        frameTwo = tkinter.Frame()
+
+        notebook1 = tkinter.ttk.Notebook(self.frame3)
+        notebook1.add(frameOne, text='选项卡1')
+        notebook1.add(frameTwo, text='选项卡2')
+
+        tkinter.Button(frameOne, text='按钮1').grid(column=1, row=1)
+        tkinter.Button(frameOne, text='按钮2').grid(column=1, row=2)
+
+        tkinter.Button(frameTwo, text='按钮1').grid(column=1, row=1)
+        tkinter.Button(frameTwo, text='按钮2').grid(column=1, row=2)
+        # entry1
+        entry1 = tk.Entry(self.frame3, textvariable=event_str, width=20, font=(15))
+        # notebook2
+        frameThree = tkinter.Frame()
+        frameFour = tkinter.Frame()
+
+        notebook2 = tkinter.ttk.Notebook(self.frame3)
+        notebook2.add(frameThree, text='类型1')
+        notebook2.add(frameFour, text='类型2')
+
+        tkinter.Button(frameThree, text='事件1').grid(column=1, row=1)
+        tkinter.Button(frameThree, text='事件2').grid(column=1, row=2)
+
+        tkinter.Button(frameFour, text='事件1').grid(column=1, row=1)
+        tkinter.Button(frameFour, text='事件2').grid(column=1, row=2)
+
+        # 功能按钮
+        tkinter.Button(self.frame3, text='退出',command=self.login_frame2).grid(column=17, row=4)
+        tkinter.Button(self.frame3, text='发布web').grid(column=17, row=5)
+        tkinter.Button(self.frame3, text='发布到屏幕2', command=self.login_frame5).grid(column=17, row=6)
+
+        # 布局
+        title.grid(column=7, row=0)
+        canvas1.grid(column=0, row=1, columnspan=16, rowspan=9)
+        notebook1.grid(column=17, row=1)
+
+        entry1.grid(column=17, row=2)
+        notebook2.grid(column=17, row=3)
+
+    def login_frame5(self):
+        print('XXXXXXXXXXXXXXXXXXX')
+        self.frame3.destroy()
+        frame5(self.root)
+
+    def login_frame2(self):
+        print('XXXXXXXXXXXXXXXXXXX')
+        self.frame3.destroy()
+        frame2(self.root)
+
+
+# 第四个页面
+class frame4:
+    def __init__(self, root):
+        self.root = root
+        self.frame4 = tk.Frame(root)
+        self.frame4.pack()
+        # 所有控件
+        event_str = tk.StringVar()
+        # 画布
+        fontStyle = tkFont.Font(family="Lucida Grande", size=20)
+        title = tk.Label(self.frame4, text='Ai视频分析系统', font=fontStyle)
+
+        canvas1 = tk.Canvas(self.frame4, width=1728, height=972, bg='white')
+        # notebook1
+        frameOne = tkinter.Frame()
+        frameTwo = tkinter.Frame()
+
+        notebook1 = tkinter.ttk.Notebook(self.frame4)
+        notebook1.add(frameOne, text='选项卡1')
+        notebook1.add(frameTwo, text='选项卡2')
+
+        tkinter.Button(frameOne, text='按钮1').grid(column=1, row=1)
+        tkinter.Button(frameOne, text='按钮2').grid(column=1, row=2)
+
+        tkinter.Button(frameTwo, text='按钮1').grid(column=1, row=1)
+        tkinter.Button(frameTwo, text='按钮2').grid(column=1, row=2)
+        # entry1
+        entry1 = tk.Entry(self.frame4, textvariable=event_str, width=20, font=(15))
+        # notebook2
+        frameThree = tkinter.Frame()
+        frameFour = tkinter.Frame()
+
+        notebook2 = tkinter.ttk.Notebook(self.frame4)
+        notebook2.add(frameThree, text='类型1')
+        notebook2.add(frameFour, text='类型2')
+
+        tkinter.Button(frameThree, text='事件1').grid(column=1, row=1)
+        tkinter.Button(frameThree, text='事件2').grid(column=1, row=2)
+
+        tkinter.Button(frameFour, text='事件1').grid(column=1, row=1)
+        tkinter.Button(frameFour, text='事件2').grid(column=1, row=2)
+
+        # 功能按钮
+        button1 = tkinter.Button(self.frame4, text='退出')
+        button2 = tkinter.Button(self.frame4, text='发布web')
+        button3 = tkinter.Button(self.frame4, text='发布到屏幕2', command=self.login_frame5)
+
+        # 布局
+        title.grid(column=7, row=0)
+        canvas1.grid(column=0, row=1, columnspan=16, rowspan=9)
+        notebook1.grid(column=17, row=1)
+
+        entry1.grid(column=17, row=2)
+        notebook2.grid(column=17, row=3)
+        button1.grid(column=17, row=4)
+        button2.grid(column=17, row=5)
+        button3.grid(column=17, row=6)
+
+    def login_frame5(self):
+        print('XXXXXXXXXXXXXXXXXXX')
+        self.frame4.destroy()
+        frame5(self.root)
+
+
+# 第五个页面
+
+class frame5:
+    def __init__(self, root):
+        self.root = root
+        self.frame5 = tk.Frame(root)
+        self.frame5.pack()
+
+        # 所有控件
+        fontStyle = tkFont.Font(family="Lucida Grande", size=20)
+        title = tk.Label(self.frame5, text='Ai视频分析系统',font = fontStyle )
+        canvas1 = tk.Canvas(self.frame5, width=1536, height=864, bg='white')
+        label1 = tk.Label(self.frame5, text='Ai分析结果显示')
+        canvas2 = tk.Canvas(self.frame5, width=320, height=180, bg='white')
+        canvas3 = tk.Canvas(self.frame5, width=320, height=180, bg='white')
+        # 布局
+
+        title.grid(column=7, row=0)
+        canvas1.grid(column=0, row=1, columnspan=16, rowspan=9)
+        label1.grid(column=18, row=1)
+
+        canvas2.grid(column=18, row=4)
+        canvas3.grid(column=18, row=7)
+
+        root.bind("<Key>", self.black)
+
+    def black(self,event):
+        if event.keycode == 27:
+            self.frame5.destroy()
+            frame3(self.root)
+
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    master(root)
+    root.mainloop()
