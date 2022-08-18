23470c1d700a094cbd271801aa26e2705b254c78
linai <918940331@qq.com>
2022-08-18 14:51:27 +0800

    1

--- imageProcessing/image_processing.py
+++ imageProcessing/image_processing.py
@@ -3,6 +3,8 @@ import numpy as np
 import PIL
 from PIL import Image, ImageDraw, ImageFont
 from PIL import Image, ImageTk
+
+
 # 中文显示
 def cv2ImgAddText(img, text, left, top, textColor=(0, 255, 0), textSize=20):
     if (isinstance(img, np.ndarray)):  # 判断是否OpenCV图片类型
@@ -13,9 +15,10 @@ def cv2ImgAddText(img, text, left, top, textColor=(0, 255, 0), textSize=20):
     draw.text((left, top), text, textColor, font=fontText)
     return cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
 
+
 # tk.canvas显示
-def cv2img_to_tkimg(im,width,height):
-    im = cv2.resize(im,(width,height))
+def cv2img_to_tkimg(im, width, height):
+    im = cv2.resize(im, (width, height))
     cvimage = cv2.cvtColor(im, cv2.COLOR_BGR2RGBA)
     pilImage = PIL.Image.fromarray(cvimage)
     tkImage = PIL.ImageTk.PhotoImage(image=pilImage)
